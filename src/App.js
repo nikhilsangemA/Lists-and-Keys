3b32f7f0ba4db871812f975d4c5947a9642d638f
nikhilsangemA <sangemnikhil15@gmail.com>
2023-10-16 13:22:22 +0000

    Lists and Keys

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import UserProfile from './components/UserProfile'
+import './App.css'
+
+const userDetails = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Nikhil',
+    role: 'SoftWare Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Monika',
+    role: 'Front End Developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Sai lisa',
+    role: 'Back End Developer',
+  },
+]
+
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">User List</h1>
+    <p>
+      {userDetails.map(eachItem => (
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
+      ))}
+    </p>
+  </div>
+)
+
+export default App
